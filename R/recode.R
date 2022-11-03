dc9b68e26958d7ce53fdc1e14a81145c849a6a37
Gjalt-Jorn Peters <gjalt-jorn@behaviorchange.eu>
2022-11-03 14:20:03 +0000

    version 0.3.10

--- R/recode.R
+++ R/recode.R
@@ -13,6 +13,18 @@
 #' @param levels an optional argument specifying the order of the levels
 #' in the returned factor; the default is to use the sort order of the
 #' level names.
+#' @param to.value The operator to separate old from new values, "=" by
+#' default; some other possibilities: "->", "~", "~>". Cannot include the
+#' interval operator (by default :) or the separator string (by default, ;),
+#' so, e.g., by default ":=>" is not allowed. The discussion in Details
+#' assumes the default "=". Use a non-default to.value if factor levels
+#' contain =.
+#' @param interval the operator used to denote numeric intervals, by
+#' default ":". The discussion in Details assumes the default ":". Use a
+#' non-default interval if factor levels contain :.
+#' @param separator the character string used to separate recode
+#' specifications, by default ";". The discussion in Details assumes the
+#' default ";". Use a non-default separator if factor levels contain ;.
 #'
 #' @export
 #' @author John Fox \email{jfox@@mcmaster.ca}
